c2f03499e631f1f447acc13a9190405323c20102
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-10-31 20:59:53 +0300

    Third commit
    Арифметические операции

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello world!!!!!')
+//Базовые арифметические операции
+
+const width = 10;
+const height = 5;
+const space = width * height;
+const newWidth = width - 4;
+const newHeight = height + 4;
+const volume = 2 ** 3;
+console.log(volume);
+
+//Строки
+const city = 'Москва';
+const street = 'Новослободская';
+console.log(city + ', ' + street + ', 5');
