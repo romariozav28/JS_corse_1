181e55270485abe825df666474f18c348ac25448
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-01 11:55:14 +0300

    if_else

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-console.log(Boolean(0));
-console.log(Boolean(''));
-console.log(Boolean(undefined));
-console.log(Boolean(null));
-console.log(Boolean(NaN));
+const money = 10;
+
+if (money > 50) {
+    console.log('Может купить наш продукт');
+} else if (money > 5) {
+    console.log('Куплен мини продукт');
+} else {
+    console.log('Не хватает баланса');
+}
+console.log('Итог');
