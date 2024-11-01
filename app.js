387fccb8f4ca155669b422c8c9de2ff686d8a348
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-01 08:53:56 +0300

    Преобразование типов

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const projectName = 'Сайт магазина';
-const price = 2000;
-const author = 'Василий Пупкин';
+const age = '18';
+console.log(Number(age) + 5);
+console.log(age * 3);
 
-const template = `${author} заказал ${projectName} по цене ${price}$.`;
-console.log(template);
+const userName = 'Вася';
+console.log(Number(userName) + 5);
+console.log(typeof NaN);
+console.log(String(4) + 7);
+console.log(Boolean('') + 10);
+console.log(true + 2);
 
-const template2 =
-`Проект
-Цена: ${price}
-`;
-console.log(template2);
+const a = 2 + '10';
+console.log(a - 10);
