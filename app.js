fb6e3e3daad7b9b99203cc0e13720ae409899947
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-04 09:50:43 +0300

    тернарные операторы

--- app.js
+++ app.js
@@ -1,39 +1,6 @@
-const role = 'admin';
+const bmwX3price = 100000;
+const fordFocusprice = 20000;
+const budget = 20000;
 
-switch(role) {
-    case 'manager':
-        console.log('Менеджер');
-        break;
-    case 'admin':
-        console.log('Админ');
-        break;
-    case 'ceo':
-        console.log('СЕО');
-        break;
-    default:
-        console.log('Мы тебя не знаем');
-}
-
-switch(role) {
-    case 'manager':
-    case 'admin':
-        console.log('не руководитель');
-        break;
-    case 'ceo':
-        console.log('Руководитель');
-        break;
-    default:
-        console.log('Мы тебя не знаем');
-}
-
-const num = 0;
-switch(true) {
-    case num > 0:
-        console.log('Положительное');
-        break;
-    case num < 0:
-        console.log('Отрицательное');
-        break;
-    default:
-        console.log('Ноль');
-}
+let message = budget > bmwX3price ? 'BMW' : 'Велосипед';
+console.log(`Я хочу купить ${message}`);
