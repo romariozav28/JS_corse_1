7261dc4a494d23e5e2d2f84a3ab2b19a543f0e2c
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-04 09:37:40 +0300

    switch

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-const secretNumber = '7';
+const role = 'admin';
 
-if(secretNumber === 7) {
-    console.log('Угадал строго');
+switch(role) {
+    case 'manager':
+        console.log('Менеджер');
+        break;
+    case 'admin':
+        console.log('Админ');
+        break;
+    case 'ceo':
+        console.log('СЕО');
+        break;
+    default:
+        console.log('Мы тебя не знаем');
 }
 
-//преведение типа чтобы выполнилось строгое равенство
-if(Number(secretNumber) === 7) {
-    console.log('Угадал строго');
+switch(role) {
+    case 'manager':
+    case 'admin':
+        console.log('не руководитель');
+        break;
+    case 'ceo':
+        console.log('Руководитель');
+        break;
+    default:
+        console.log('Мы тебя не знаем');
 }
 
-if(secretNumber == 7) {
-    console.log('Угадал не строго');
+const num = 0;
+switch(true) {
+    case num > 0:
+        console.log('Положительное');
+        break;
+    case num < 0:
+        console.log('Отрицательное');
+        break;
+    default:
+        console.log('Ноль');
 }
