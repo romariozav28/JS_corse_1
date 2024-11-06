e0f9a3602c227690faa2e05ff490dbf9c52a7107
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-06 09:27:35 +0300

    Оператор нулевого сравнения

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-console.log('Вася' || 'Олег');
-console.log(false || 'Олег');
-console.log('Вася' || false);
-console.log(false || false);
-console.log(true || 'Олег');
-console.log('Вася' || true);
-
-console.log('Вася' && 'Олег');
-console.log(false && 'Олег');
-console.log('Вася' && false);
-console.log(false && false);
-console.log(true && 'Олег');
-console.log('Вася' && true);
-
-let a = 'Марина';
-const userName = a || 'Петя';
-console.log(userName);
-
-const isAdmin = false;
-const fileName = isAdmin && 'file.mp4';
-console.log(fileName);
+let age = 0;
+console.log(age || 18);
+console.log(age ?? 18);
