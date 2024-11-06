621760fbbc45f87bbaef8b3d8903dae25ec32907
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-06 09:51:11 +0300

    Задание на булеву логику магазин

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-let age = 0;
-console.log(age || 18);
-console.log(age ?? 18);
+let balance = 1000;
+let bonus = 100;
+let isBanned = false;
+let isExist = false;
+let isSelling = true;
+
+const canBuy = (balance>1000 || bonus>100) && !isBanned && !isExist && isSelling;
+
+console.log(canBuy ? 'Можно купить' : 'Нельзя купить');
