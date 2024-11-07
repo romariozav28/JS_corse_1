d26eed02dff43d1566cbbae0784b1d3ad8152ab8
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-07 17:48:10 +0300

    Анонимные функции

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-function logName(name, surname) {
-    console.log(`Меня зовут ${name} ${surname}`);
+function powerOfTwo(num) {
+    return num * num;
 }
 
-logName('Крокс', 'Петров');
-
-
-function countDepositSum (depositInUSD, month, rate) {
-    return depositInUSD * (1 + rate/100/12) ** month;
-}
-
-const example1 = countDepositSum(1000, 24, 12);
-console.log(example1);
-
-console.log(countDepositSum(1000, 48, 10));
+console.log(powerOfTwo(5));
