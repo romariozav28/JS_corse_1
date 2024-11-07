fe7daf96568764987c781e12fbb96ede5bd85196
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-07 17:56:58 +0300

    Стрелочные функции

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 function powerOfTwo(num) {
+    console.log(num);
     return num * num;
 }
 
 console.log(powerOfTwo(5));
+
+const poft = (num, num2) => {
+    console.log(`${num} ${num2}`);
+    return num * num2;
+}
+console.log(poft(6, 3));
