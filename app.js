34fcd77b7f00ad467d41890614394d08cda2fdcb
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-09 10:32:33 +0300

    Функции в функциях

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-function canAccesWebsite(age){
-    if(age < 18) {
-        return false;
-    }
-    return true;
-}
-console.log(canAccesWebsite(18));
+const KG_IN_USD = 7;
+const KM_IN_USD = 5;
 
-const cAw = (age) => age>=18;
-console.log(cAw(17));
+function calculateW(present) {
+    return present * KG_IN_USD;
+}
+function calculateKm(distance) {
+    return distance * KM_IN_USD;
+}
+function getExchangePrice(present1, present2, distance) {
+    const price1 = calculateW(present1);
+    const price2 = calculateW(present2);
+    const distancePrice = calculateKm(distance);
+    return price1 + price2 + distancePrice;
+}
+
+console.log(getExchangePrice(1, 2, 7))
