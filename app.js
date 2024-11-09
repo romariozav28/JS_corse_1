125901aed9b8f888b98d77fa15459ba819c28fed
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-09 11:20:07 +0300

    Упражнение комп в кредит

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const KG_IN_USD = 7;
-const KM_IN_USD = 5;
+const age = 23;
+const isActive = false;
+const cash = 1900;
 
-function calculateW(present) {
-    return present * KG_IN_USD;
-}
-function calculateKm(distance) {
-    return distance * KM_IN_USD;
-}
-function getExchangePrice(present1, present2, distance) {
-    const price1 = calculateW(present1);
-    const price2 = calculateW(present2);
-    const distancePrice = calculateKm(distance);
-    return price1 + price2 + distancePrice;
+
+function isCredit(age, isActive) {
+    return age > 24 ? isActive ? 500 : 100 : 0;
 }
 
-console.log(getExchangePrice(1, 2, 7))
+function isBuy(age, isActive, cash){
+    const credit = isCredit(age, isActive);
+    return (credit + cash) >= 2000 ? 'Может купить' : 'Не может купить';
+}
+
+console.log(isBuy(age, isActive, cash));
