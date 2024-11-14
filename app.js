731f0f3128052d845b8b140d4843ec9ad8479642
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-14 11:59:47 +0300

    Деструктуризация

--- app.js
+++ app.js
@@ -1,34 +1,9 @@
-const tasks = ['Задача 1'];
+const userData = ['Антон', 18, 'Москва'];
 
-function addTask(arrName, nameTask) {
-    arrName.push(nameTask);
+function getData() {
+    return ['Антон', 18, 'Москва'];
 }
-addTask(tasks, 'Задача 2');
-addTask(tasks, 'Задача 3');
-addTask(tasks, 'Задача 4');
-addTask(tasks, 'Задача 5');
-console.log(tasks);
 
+const [userName, _, city] = userData;
 
-function deleteTask(arrName, nameTask){
-    const index = arrName.indexOf(nameTask);
-    if(index === -1) {
-        return;
-    }
-    return arrName.splice(index, 1);
-}
-deleteTask(tasks, 'Задача 4');
-console.log(tasks);
-
-
-function replaceMember(arrName, nameTask){
-    const result = deleteTask(arrName, nameTask);
-    if(result) {
-        tasks.unshift(result[0]);
-    }
-}
-replaceMember(tasks, 'Задача 4');
-console.log(tasks);
-
-
-
+console.log(userName, city);
