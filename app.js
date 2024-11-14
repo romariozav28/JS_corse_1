7dd90379217158cdb4df9c0ea9794e7a7e794ecd
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-14 11:38:45 +0300

    Задача члены массива

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-const roles = ['user', 'admin', 'manager', 'superuser'];
+const tasks = ['Задача 1'];
 
-const url = 'auth/user/login';
-const res = url.split('/');
-console.log(res);
+function addTask(arrName, nameTask) {
+    arrName.push(nameTask);
+}
 
-console.log(roles.join(' -> '));
+addTask(tasks, 'Задача 2');
+addTask(tasks, 'Задача 3');
+addTask(tasks, 'Задача 4');
+addTask(tasks, 'Задача 5');
+console.log(tasks);
+
+function deleteTask(arrName, nameTask){
+    const index = arrName.indexOf(nameTask);
+    if(index === -1) {
+        return;
+    }
+    arrName.splice(index, 1);
+}
+deleteTask(tasks, 'Задача 3');
+console.log(tasks);
+
+function replaceMember(arName, nameTask){
+    const index = tasks.indexOf(nameTask);
+    if(index === -1) {
+        return;
+    }
+    const res = tasks.splice(index, 1);
+    tasks.unshift(res.join());
+}
+replaceMember(tasks, 'Задача 5');
+console.log(tasks);
+
+
+
