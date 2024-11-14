0ff4a6e0aff79a273d1af65adbec8d6d78b5867e
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-14 11:53:13 +0300

    Принципы DRY

--- app.js
+++ app.js
@@ -3,32 +3,31 @@ const tasks = ['Задача 1'];
 function addTask(arrName, nameTask) {
     arrName.push(nameTask);
 }
-
 addTask(tasks, 'Задача 2');
 addTask(tasks, 'Задача 3');
 addTask(tasks, 'Задача 4');
 addTask(tasks, 'Задача 5');
 console.log(tasks);
 
+
 function deleteTask(arrName, nameTask){
     const index = arrName.indexOf(nameTask);
     if(index === -1) {
         return;
     }
-    arrName.splice(index, 1);
+    return arrName.splice(index, 1);
 }
-deleteTask(tasks, 'Задача 3');
+deleteTask(tasks, 'Задача 4');
 console.log(tasks);
 
-function replaceMember(arName, nameTask){
-    const index = tasks.indexOf(nameTask);
-    if(index === -1) {
-        return;
+
+function replaceMember(arrName, nameTask){
+    const result = deleteTask(arrName, nameTask);
+    if(result) {
+        tasks.unshift(result[0]);
     }
-    const res = tasks.splice(index, 1);
-    tasks.unshift(res.join());
 }
-replaceMember(tasks, 'Задача 5');
+replaceMember(tasks, 'Задача 4');
 console.log(tasks);
 
 
