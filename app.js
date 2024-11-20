a07fd852e724f0b088e8a028dc2b5a8856250342
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-20 21:41:37 +0300

    циклы for of и for in

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-/* for (let i = 1; i < 5; i++){
-    console.log(`Вывод - ${i}`);
-}
-
-let i = 1;
-while (i<5) {
-    console.log(`Вывод - ${i}`);
-    i++;
-} */
-
 const arr = [1, 4, 8, 7];
-for (let i = 0; i<arr.length; i++){
-    if(arr[i] > 5){
-        break;
-    }
-    console.log(arr[i]);
+
+for (let element of arr) {
+    console.log(element);
 }
 
-let i = 0;
-while(arr[i] <= 5 && i < arr.length){
-    console.log(arr[i]);
-    i++;
+for (let index in arr) {
+    console.log(index);
 }
-
-let j = 0;
-do {
-    console.log(j);
-    j++
-} while(j<0)
