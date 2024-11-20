bfbb75312bceeb23e39b097144707bb1698a75f6
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-20 19:01:26 +0300

    break and continue

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-for(let i = 1; i<10; i++){
-    console.log(`Наш баланс ${i}$`);
+const tasks = ['Задача 1', 'Задача 2', 'Задача 3'];
+
+for(let i = 0; i<tasks.length; i++){
+    if (tasks[i] === 'Задача 2'){
+        continue;
+    }
+    console.log(tasks[i]);
+}
+
+console.log('---------------')
+
+for(let i = 0; i<tasks.length; i++){
+    if (tasks[i] === 'Задача 2'){
+        break;
+    }
+    console.log(tasks[i]);
 }
