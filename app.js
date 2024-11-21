4bd183c5e03e0cd971e99943d44a05c752178fe4
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-21 19:34:19 +0300

    Функции высшего порядка

--- app.js
+++ app.js
@@ -1,45 +1,22 @@
-const operations = [1000, -700, 300, -500, 10000];
-const startBalance = 100;
-
-function endBalance(startBalance, operations) {
-    let endBalance = startBalance;
-    for (let element of operations) {
-        endBalance += element;
-    }
-    console.log(`Итоговый баланс составил ${endBalance}$`);
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function power(a, b){
+    return a**b;
 }
 
-function stageBalance(startBalance, operations){
-    let stageBalance = startBalance;
-    let operationCount = 0;
-    for (let element of operations) {
-        stageBalance += element;
-        operationCount ++;
-        if(stageBalance<0){
-            console.log(`При совершении ${operationCount} операции баланс стал отрицательным`);
-        }
-    }
+function calculate(a, b, fn){
+    console.log(fn.name);
+    const res = fn(a, b);
+    return res;
 }
 
-function profit(operations){
-    let profit = 0;
-    let profitCount = 0;
-    let cost = 0;
-    let costCount = 0;
-    for (let element of operations){
-        if (element < 0){
-            cost += element;
-            costCount++;
-        } else {
-            profit += element;
-            profitCount++;
-        }
-    }
-    console.log(`Средний доход составляет: ${profit/profitCount}$
-Средний расход составляет: ${cost/costCount}$`)
-}
-
-endBalance(startBalance, operations);
-stageBalance(startBalance, operations);
-profit(operations);
-
+let res = calculate(3, 5, add);
+console.log(res);
+res = calculate(3, 5, subtract);
+console.log(res);
+res = calculate(3, 5, power);
+console.log(res);
