f733498c56cefaac6c60caa539171333b3b9eaf6
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-21 19:39:06 +0300

    Возврат функции

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-function add(a, b){
-    return a + b;
-}
-function subtract(a, b){
-    return a - b;
-}
-function power(a, b){
-    return a**b;
+function power(pow){
+    return function(num){
+        return num**pow;
+    }
 }
 
-function calculate(a, b, fn){
-    console.log(fn.name);
-    const res = fn(a, b);
-    return res;
-}
+const powerOfTwo = power(2);
+console.log(powerOfTwo(5));
+console.log(powerOfTwo(10));
 
-let res = calculate(3, 5, add);
-console.log(res);
-res = calculate(3, 5, subtract);
-console.log(res);
-res = calculate(3, 5, power);
-console.log(res);
+const powerOfThree = power(3);
+console.log(powerOfThree(5));
+
+console.log(power(5)(4));
