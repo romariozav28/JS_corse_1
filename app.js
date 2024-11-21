984e606357b0f6a511f1c6920f68a969dccf6467
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-21 19:09:48 +0300

    Задание циклы

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
-const arr = [1, 4, 8, 7];
+const operations = [1000, -700, 300, -500, 10000];
+const startBalance = 100;
 
-for (let element of arr) {
-    console.log(element);
+function endBalance(startBalance, operations) {
+    let endBalance = startBalance;
+    for (let element of operations) {
+        endBalance += element;
+    }
+    console.log(`Итоговый баланс составил ${endBalance}$`);
 }
 
-for (let index in arr) {
-    console.log(index);
+function stageBalance(startBalance, operations){
+    let stageBalance = startBalance;
+    let operationCount = 0;
+    for (let element of operations) {
+        stageBalance += element;
+        operationCount ++;
+        if(stageBalance<0){
+            console.log(`При совершении ${operationCount} операции баланс стал отрицательным`);
+        }
+    }
 }
+
+function profit(operations){
+    let profit = 0;
+    let profitCount = 0;
+    let cost = 0;
+    let costCount = 0;
+    for (let element of operations){
+        if (element < 0){
+            cost += element;
+            costCount++;
+        } else {
+            profit += element;
+            profitCount++;
+        }
+    }
+    console.log(`Средний доход составляет: ${profit/profitCount}$
+Средний расход составляет: ${cost/costCount}$`)
+}
+
+endBalance(startBalance, operations);
+stageBalance(startBalance, operations);
+profit(operations);
+
