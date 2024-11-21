31c1c8f3e206b9db9a0912f75446f242f9af74ca
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-21 19:56:21 +0300

    задача функции высшего порядка - стрелочные функции

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function power(pow){
+/* function power(pow){
     return function(num){
         return num**pow;
     }
@@ -11,4 +11,7 @@ console.log(powerOfTwo(10));
 const powerOfThree = power(3);
 console.log(powerOfThree(5));
 
-console.log(power(5)(4));
+console.log(power(5)(4)); */
+
+const res = pow => num => num**pow;
+console.log(res(5)(2));
