59813fcba5a95aeaba1459fa967f2c9f16a0508b
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-24 10:23:27 +0300

    flat flatMap

--- app.js
+++ app.js
@@ -1,38 +1,6 @@
-const arr = [1, 4, 4, 10, 20];
+const prices = [[2, 4], [3, 4], [10, [20, 50]]];
 
-function some(array, number){
-    let flag = false;
-    for(let i = 0; i < array.length; i++){
-        if(array[i] === number){
-            flag = true;
-            break;
-        }
-    }
-    return flag;
-}
-
-function some_find(array, number){
-    let flag = array.find(element => element === number);
-    if (flag != undefined) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function some_find1(array, number){
-    let flag = Boolean(array.find(element => element === number));
-    return flag;
-}
-
-function some_find2(array, number){
-    let flag = array.find(element => element === number);
-    return flag != undefined ? true : false;
-}
-
-console.log(some(arr, 4));
-console.log(some_find(arr, 4));
-console.log(some_find1(arr, 0));
-console.log(some_find2(arr, 4));
-console.log(arr.includes(4));
-console.log(arr.some(el => el === 4));
+const res = prices.flat(2);
+console.log(res);
+const res2 = prices.flatMap(el => el.concat('!'));
+console.log(res2);
