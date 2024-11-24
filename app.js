4aeed270c9112eeab9febe9033df59c68b8e19c0
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-24 09:56:39 +0300

    some наличие элемента в массиве и др

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 const arr = [1, 4, 4, 10, 20];
 
-let elGT5 = arr.find(el => el > 5);
-let elGT5Index = arr.findIndex(el => el > 5);
-console.log(elGT5);
-console.log(elGT5Index);
+function some(array, number){
+    let flag = false;
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === number){
+            flag = true;
+            break;
+        }
+    }
+    return flag;
+}
+
+function some_find(array, number){
+    let flag = array.find(element => element === number);
+    if (flag != undefined) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function some_find1(array, number){
+    let flag = Boolean(array.find(element => element === number));
+    return flag;
+}
+
+function some_find2(array, number){
+    let flag = array.find(element => element === number);
+    return flag != undefined ? true : false;
+}
+
+console.log(some(arr, 4));
+console.log(some_find(arr, 4));
+console.log(some_find1(arr, 0));
+console.log(some_find2(arr, 4));
+console.log(arr.includes(4));
+console.log(arr.some(el => el === 4));
