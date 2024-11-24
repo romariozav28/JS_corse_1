e79df3194417dce84417b1acd895a748c483c46c
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-24 10:53:07 +0300

    быстрое создание массивов

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const users = ['Вася', 'Маша', 'Катя', 'Аня'];
-console.log(users);
-users.sort();
-console.log(users);
+const arr = [1, 2, 3, 4, 5];
 
-const operations = [100, -300, -100, 50, 480];
-console.log(operations);
+console.log(new Array(1, 2, 3, 4, 5));
+const arr2 = new Array(5);
+console.log(arr2);
+arr2.fill(1, 0, 3);
+arr2.fill(2, 2, 5);
+console.log(arr2);
 
-operations.sort((a, b) => a - b);
-console.log(operations);
-
-operations.sort((a, b) => b - a);
-console.log(operations);
+const arr3 = Array.from({length:5}, (_, i) => i+1);
+console.log(arr3);
