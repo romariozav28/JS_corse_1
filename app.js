ae76ed74b589e193ce47e62990f7c83926d6bfbd
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-24 09:02:47 +0300

    reduce

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const prices = [[100, 200], [120, 100], [200, 350]];
-console.log(prices);
+const operations = [100, 20, 7, 30, 50];
 
-const negativePrices = prices
-.map(product => product[1] - product[0])
-.filter(price => price > 0)
-;
+let balance = 0;
+for(const operation of operations){
+    balance += operation;
+    
+}
+console.log(balance);
 
-console.log(negativePrices);
+const finalBalance = operations.reduce((acc, operation, i) => {
+    console.log(`Итерация ${i}, acc: ${acc}, operation ${operation}`)
+    return acc += operation;
+}, 0);
+console.log(finalBalance);
+
+const minElement = operations.reduce((acc, element) => {
+    if(element > acc){
+        return acc;
+    } else return element; 
+});
+
+console.log(minElement);
