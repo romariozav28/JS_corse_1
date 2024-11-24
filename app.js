b04fa33382455fab9961c1774766143dd1787164
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-24 09:26:36 +0300

    Задача reduce

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-const operations = [100, 20, 7, 30, 50];
+const arr = [1, 4, 4, 10];
 
-let balance = 0;
-for(const operation of operations){
-    balance += operation;
+const avg = arr.reduce((acc, number, index, array) => {
+    if(index != array.length - 1){
+        return acc += number;
+    } else {
+        acc += number;
+        return acc/array.length;
+    }
     
-}
-console.log(balance);
+}, 0)
 
-const finalBalance = operations.reduce((acc, operation, i) => {
-    console.log(`Итерация ${i}, acc: ${acc}, operation ${operation}`)
-    return acc += operation;
-}, 0);
-console.log(finalBalance);
-
-const minElement = operations.reduce((acc, element) => {
-    if(element > acc){
-        return acc;
-    } else return element; 
-});
-
-console.log(minElement);
+console.log(avg);
