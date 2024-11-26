86288aec4ded14d31b8bd2a26c568ecc52a78645
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-26 22:48:11 +0300

    упражнение кошелек

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const user = {
-    name: 'Вася',
-    surname: 'Пупкин',
-    age: 24,
-    getFullName: function(){
-        return this.name + ' ' + this.surname;
+const wallet = {
+    balance: 0,
+    operations: [],
+    operation: function(reason, sum){
+        if(this.balance + sum < 0 ) {
+            console.log(`Операция по ${reason} на сумму ${sum} не выполнена. Недостаточно средств`);
+        } else {
+        this.operations.push({'reason': reason, 'sum': sum});
+        console.log(`Операция по ${reason} на сумму ${sum} выполнена успешно`);
+        return this.balance += sum;
+        }
+    },
+    countOperations: function(){
+        return this.operations.length;
     }
 };
 
-console.log(user.getFullName());
+wallet.operation('Получение выигрыша', 200);
+wallet.operation('Уплата налогов', -100);
+console.log(wallet);
+console.log(wallet.countOperations());
