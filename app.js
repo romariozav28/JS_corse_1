f4db081e959916ce4008e9abc0efe567ef3c274c
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-26 15:42:27 +0300

    обращение к элементам

--- app.js
+++ app.js
@@ -3,5 +3,22 @@ const user = {
     name: 'Вася',
     surname: 'Пупкин',
     age: 24,
-    skills: []
+    skills: ['Программирование', 'Готовка'],
+    eduBasic: 'Школа 10',
+    eduPro: 'МФТИ'
 }
+
+console.log(user);
+console.log(user.skills);
+console.log(user.skills[0]);
+console.log(user['skills']);
+console.log(user['skills'][0]);
+
+const level = 'Pro';
+console.log(user['edu' + level]);
+
+/* const res = prompt('Введите свойство');
+console.log(user[res]); */
+
+user['city'] = 'Москва';
+console.log(user);
