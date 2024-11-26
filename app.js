b7467ff5f7d5b0f814c1160a5da3ea3b916d76bc
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-26 21:54:03 +0300

    методы объектов

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
-const users = [
-    {
-        name: 'Вася',
-        surname: 'Пупкин',
-        age: 30,
-        skills: ['Разработка', 'DevOps']
-    },
-    {
-        name: 'Катя',
-        surname: 'Белова',
-        age: 18,
-        skills: ['Дизайн']
+const user = {
+    name: 'Вася',
+    surname: 'Пупкин',
+    age: 24,
+    getFullName: function(){
+        return this.name + ' ' + this.surname;
     }
-];
+};
 
-const userData = users.map(user => {
-    return {
-        fullName: `${user.name} ${user.surname}`,
-        skillNum: user.skills.length
-    };
-});
-
-users.forEach(el => {
-    el['fullName'] = `${el['name']} ${el['surname']}`;
-    el['skills'] = el.skills.length;
-    delete el.name;
-    delete el.age;
-    delete el.surname;
-});
-console.log(users);
-console.log(userData);
+console.log(user.getFullName());
