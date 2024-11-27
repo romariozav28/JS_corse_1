a93d337e06e2fc1bfc3bffdb4a0f041e3aed5eac
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-27 09:07:57 +0300

    итеррирование по объекту

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-const wallet = {
-    balance: 0,
-    operations: [],
-    operation: function(reason, sum){
-        if(this.balance + sum < 0 ) {
-            console.log(`Операция по ${reason} на сумму ${sum} не выполнена. Недостаточно средств`);
-        } else {
-        this.operations.push({'reason': reason, 'sum': sum});
-        console.log(`Операция по ${reason} на сумму ${sum} выполнена успешно`);
-        return this.balance += sum;
-        }
+const cities = {
+    msk: {
+        let: 200,
+        temp: 25
     },
-    countOperations: function(){
-        return this.operations.length;
+    spb: {
+        let: 100,
+        temp: 20
     }
 };
 
-wallet.operation('Получение выигрыша', 200);
-wallet.operation('Уплата налогов', -100);
-console.log(wallet);
-console.log(wallet.countOperations());
+let sumTemp = 0;
+let citiesCount = 0;
+for(const key in cities){
+    sumTemp += cities[key].temp;
+    citiesCount++;
+}
+console.log(sumTemp/citiesCount);
+
+let sumTemp2 = 0;
+let citiesCount2 = Object.keys(cities).length;
+for(const key of Object.keys(cities)){
+    sumTemp2 += cities[key].temp;
+}
+console.log(sumTemp2/citiesCount2);
