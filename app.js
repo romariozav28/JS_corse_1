491914b65e0e4389ce9b4e13efa82a5e2d0f524f
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-27 09:48:33 +0300

    optional chaining

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
-let user = {
-    name: 'Вася',
-    age: 40,
-    city: 'Moscow'
+const cities = {
+    msk: {
+        temp: {
+            celsius: 25
+        }
+    },
+    spb: {
+
+    }
+}
+const city = 'spb';
+if(cities[city] != undefined && cities[city].temp != undefined){
+    console.log(cities[city].temp.celsius);
 }
 
-const {age, ...userWithoutAge} = user;
-console.log(age);
-console.log(userWithoutAge);
-
-const additionalData = {
-    skills: ['Разработка', 'Дизайн'],
-    creditCard: '2343-9876-9034-6745'
-};
-
-//добавление новых свойств объекта работет даже если исходный объект как константа
-/* user.skills = additionalData.skills;
-user.creditCard = additionalData.creditCard;
-console.log(user); */
-
-//добавление свойства - работает даже если исходный объект как константа
-//user.test = 'ljasbvnkadjn';
-
-//работает только если исходный объект let
-user = {
-    ...user, //взяли все из объекта user
-    ...additionalData //взяли все из объекта additionalData
-}
-console.log(user);
+console.log(cities[city]?.temp?.celsius);
