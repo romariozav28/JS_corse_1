e55c6f8256148bf53d4bbf5989900d1b4dc790d3
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-11-29 23:40:33 +0300

    задача склад

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
-const cities = {
-    msk: {
-        temp: {
-            celsius: 25
+const warehouse = {
+    goods: [],
+
+    findGoodById: function (id) {
+        return this.goods.find(g => g.id == id)
+        },
+
+    addGood: function (good) {
+        const existGood = this.findGoodById(good.id)
+        if(existGood) {
+            console,log('Этот товар уже есть на складе');
         }
+        this.goods.push(good);
     },
-    spb: {
-
+    getWeightKg: function () {
+        return this.goods.reduce((acc, el) => 
+        acc += el.weight?.kg ? el.weight.kg : 0,
+    0)
     }
-}
-const city = 'spb';
-if(cities[city] != undefined && cities[city].temp != undefined){
-    console.log(cities[city].temp.celsius);
-}
+};
 
-console.log(cities[city]?.temp?.celsius);
+const car = {
+    id: 1,
+    weight: {
+        kg: 100
+    },
+    brand: 'Ford'
+};
+const chair = {
+    id: 2,
+    weight: {
+        kg: 2
+    }
+};
+const paper = {
+    id: 3,
+    color: 'red'
+};
+
+warehouse.addGood(car);
+warehouse.addGood(chair);
+warehouse.addGood(paper);
+console.log(warehouse);
+console.log(warehouse['goods']);
+console.log(warehouse.findGoodById(4));
