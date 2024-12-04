f80b9b05bc8ec5883a7e53b3bed4a5b0b150c622
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-12-04 09:41:56 +0300

    пример поднятия

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 'use strict'
 
-const user = {
-    name: 'Иван',
-    roles: []
-};
 
-function addRole(){
-    const role = 'test';
-    user.roles.push(role);
-    let successMessage = 'Ура';
-    //const role = 'test'; - ошибка, переменная объявлена и присвоена после использования
-    console.log(successMessage);
+//console.log(a); - выведет ошибку
+console.log(b);// b будет undefind
+const a = 3;
+var b = 2;
+console.log(a);// ошибки не будет так как вызов после объявления
+console.log(b);// b будет со значением 2
+
+addUser();//можно функцию вызвать и до объявления
+function addUser(){
+    //console.log('User added');
 }
+addUser();//можно функцию вызвать и после объявления, лучше после
 
-addRole();
-console.log(user);
+//arr1();//- ошибка вызов стрелочной функции до объявления переменная const,
+//так же будет и с var, так как var при вызове до объявления undefined
+const arr1 = () => {
+    console.log('arr1');
+};
+arr1();//вызов функции после объявления const
