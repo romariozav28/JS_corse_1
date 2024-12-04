11e70566d76ce7ed20986f4851f9ccf138a1e994
romariozav28 <rudolf.petrov.44@yandex.ru>
2024-12-04 19:40:40 +0300

    контекст в методах

--- app.js
+++ app.js
@@ -1,38 +1,25 @@
 'use strict';
 
-//console.log(this);
-
-function addNum(num1, num2){
-    console.log(this);
-    return num1 + num2;
-}
-
-const addNum2 = (num1, num2) => {
-    console.log(this);
-    return num1 + num2;
-};
-
 const user = {
     name: 'Вася',
     surname: 'Пупкин',
-    getFullName: function(){
+    age: 20,
+    getUserInfo: function(){
         console.log(this);
-        return this.name + ' ' + this.surname;
+        console.log(`${this.name} + ${this.surname}`);
+        const canDrink = () => {
+            if(this.age >= 18){
+                console.log('Может уже пить');
+            } else {
+                console.log('Не может еще пить');
+            }
+        };
+        canDrink();
+    },
+    getUserInfoArrow:() => {
+        console.log(`${this.name} ${this.surname}`);
     }
 };
-user.getFullName();
 
-const user2 = {
-    name: 'Марина',
-    surname: 'Катц'
-}
-user2.getFullName = user.getFullName;
-user2.getFullName();
-
-console.log(user.getFullName());
-console.log(user2.getFullName());
-
-const getFullName = user2.getFullName();
-
-
-console.log(addNum2(2, 3));
+user.getUserInfo();
+user.getUserInfoArrow();
